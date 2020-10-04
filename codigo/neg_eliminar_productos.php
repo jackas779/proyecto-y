bc3306d7c41a3b8d35e0ec071b5c75efc02dfe0c
Nicolas Ardila <jackas779@gmail.com>
2020-10-04 18:05:30 -0500

    Se creo CRUD completo de los productos

--- codigo/neg_eliminar_productos.php
+++ codigo/neg_eliminar_productos.php
@@ -1,8 +1,28 @@
 <?php 
 include("conexion.php");
-if(isset ($_GET["id"])){
-    $variable=$_GET["id"];
-    print_r($variable);
+    class Producto {
+    public function eliminar($cod_producto, $id_producto){
+        include("conexion.php");
+        $contador="0";
+        // se consulta primero si la categoria ya existe
+        $consulta = "SELECT * FROM productos WHERE cod_producto='$cod_producto'";
+    if(!$resultado = $db->query($consulta)){
+        die('hay un error con la consulta o los datos no existen vuelve a comprobar !!![' . $db->error . ']');
+        }// la consulta       
+    while($fila = $resultado->fetch_assoc()){
+        $bcod_producto=stripslashes($fila["cod_producto"]);
+        $bid_producto=stripslashes($fila["id_producto"]);
+        }// la consulta termina
+    if($bid_producto==$id_producto){
+        mysqli_query($db,"DELETE FROM productos WHERE id_producto='$id_producto'") or die (mysqli_error($db));header("location: pre_consultar_productos.php");
+        }
+    if($bid_producto!=$id_producto){
+        echo"No se actualizo el producto";
+        }    
+    }
+    
 }
+$nuevo=new Producto();
+$nuevo->eliminar($_POST["el_cod_producto"],$_POST["id_producto"]);
 
 ?>
--- codigo/pre_consultar_productos.php
+++ codigo/pre_consultar_productos.php
@@ -3,10 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Document</title>
     <link rel="stylesheet" href="../estilos/estilos.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
 <div id="banner">
@@ -49,42 +47,21 @@ $consulta = "SELECT P.id_producto, P.cod_producto, P.producto, P.descripcion, P.
         $bestado=stripslashes($fila["nombre_estado"]);
         $bcategoria=stripslashes($fila["categoria"]);
 
-
         echo "<tr>";
         echo "<td>$bproducto</td>";
         echo "<td>$bdescripcion</td>";
         echo "<td>$bcod_producto</td>";
-        echo "<td><button><a href='neg_cambio_estado.php?id=$bid_producto'>cambiar estado</a></button></td>";
+        echo "<td>$bestado<button><a href='neg_cambio_estado.php?id=$bid_producto'>cambiar estado</a></button></td>";
         echo "<td><button><a href='pre_editar_productos.php?id=$bid_producto'>Editar</a></button></td>";
-        echo "<td><button class='btn btn-danger btn-sm' type='button' data-toggle='modal' data-target='#modal1'>Eliminar</button></td>";
+        echo "<td><button><a href='pre_eliminar_productos.php?id=$bid_producto'>Eliminar</a></button></td>";
         echo "<td>$bcategoria</td>";
-        echo "<td>";
-        
-        //modal
-        echo"<div class='container'>
-        <div class='modal fade' tabindex='-1' id='modal1'>
-            <div class='modal-dialog'>
-                <div class='modal-content'>
-                    <div class='modal-body'>
-                        ¿ Esta seguro de esta desición ?
-                    </div>
-                    <div class='modal-footer'>
-                        <a href='neg_eliminar_productos.php?id=$bid_producto' class='btn btn-primary' >Aceptar</a>
-                        <button class='btn btn-danger' data-dismiss='modal' >Cancelar</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>";
-        echo "</td>";
         echo "<tr>";
+
         }
  
-    echo "</table>";
+    echo "</table>";    
 
-   
 ?>
-
 </div><!-- El cuerpo de la pagina, se cambia o modifica de acuerdo la pagina. -->
 
 <div id="col2">
@@ -95,21 +72,6 @@ $consulta = "SELECT P.id_producto, P.cod_producto, P.producto, P.descripcion, P.
 <div id="footer">
 <?php include("footer.php") ?>
 </div> <!-- Se llama al footer -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-<script type="text/javascript">
-
-$(function(){
-    $('#modal1').modal({
-        backdrop:'static',
-        keybord: false,
-        focus: false,
-        show:false
-    });
-});
-
-</script>
 
 </body>
 </html>
